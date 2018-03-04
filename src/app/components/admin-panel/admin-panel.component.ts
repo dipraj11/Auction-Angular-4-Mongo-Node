05c8ad4d35f181b6686952d5822e5d452ed81258
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-03-04 21:53:54 +0530

    added admin panel

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {timer} from 'rxjs/observable/timer'
+import {take,map} from 'rxjs/operators'
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
+  timeLeft
+  baseTime = 16
+  bidSealed: boolean = true
+
+  newBid: boolean
   constructor() { }
 
   ngOnInit() {
+    this.newBid = true
+  }
+
+  startBid() {
+    this.newBid = false
+    this.timeLeft = timer(0, 1000).pipe(take(this.baseTime), map(() => --this.baseTime))
   }
 
 }
