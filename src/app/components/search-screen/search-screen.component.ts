15acd6ed0c3813e3bdfdc86b55c78bcfc388fc2c
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-03-04 19:41:30 +0530

    player search page implemented

--- src/app/components/search-screen/search-screen.component.ts
+++ src/app/components/search-screen/search-screen.component.ts
@@ -5,17 +5,67 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './search-screen.component.html',
   styleUrls: ['./search-screen.component.sass']
 })
+
 export class SearchScreenComponent implements OnInit {
-  users = [{
-    id:'1',
+
+  players = [{
     name: 'Shishir Tiwari',
-    creation:'2017-09-01',
-    color: 'red'
-    
-  }]
+    batting: true,
+    bowling: true,
+    fielding: false,
+    basePrice: 50,
+    sold: false,
+    team: '',
+
+
+  },
+  {
+    name: 'Rajesh Singh',
+    batting: true,
+    bowling: true,
+    fielding: false,
+    basePrice: 50,
+    sold: false,
+    team: '',
+
+
+
+  },
+  {
+    name: 'Vishesh Harwani',
+    batting: true,
+    bowling: true,
+    fielding: false,
+    basePrice: 50,
+    sold: false,
+    team: '',
+
+
+
+  }
+  ]
+  nHeads: any = []
+  enums: any = {
+    name: 'Name',
+    team: 'Team',
+    sold: 'Status',
+    basePrice: 'Base Price',
+    batting: 'Batting',
+    bowling: 'Bowling',
+    fielding: 'Fielding',
+  }
+  keys
+  soldStatus = {
+    true: 'Sold',
+    false: 'Unsold'
+  }
   constructor() { }
 
   ngOnInit() {
+    this.keys = Object.keys(this.players[0])
+    for (let i = 0; i < this.keys.length; i++) {
+      this.nHeads[i] = this.enums[this.keys[i]]
+    }
   }
 
 }
